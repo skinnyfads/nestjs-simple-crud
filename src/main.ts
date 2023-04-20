a859afe88e11cea2c75f01e8db8d52c73451b55d
skinnyfads <skinnyfads@proton.me>
2023-04-21 08:13:31 +0900

    feat: add custom start log

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { join } from 'node:path';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
@@ -15,6 +17,7 @@ async function bootstrap() {
     },
   );
   await app.listen();
+  logger.log('Microservice is listening...');
 }
 
 bootstrap();
