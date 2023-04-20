914bf5d20ae43dc999f1aae9c459f93430e8b228
skinnyfads <skinnyfads@proton.me>
2023-04-21 04:32:51 +0900

    feat: setting up grpc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import { join } from 'node:path'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'team',
+        protoPath: join(__dirname, './teams/team.proto')
+      },
+    },
+  );
+  await app.listen()
 }
+
 bootstrap();
